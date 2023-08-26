e360b41669d5fdf2af392da4fed6c512930bcbee
LauraChacon <laurachacon.ve@gmail.com>
2023-08-26 00:12:55 -0500

    Cambio v2

--- main.js
+++ main.js
@@ -1,69 +1,96 @@
-import { obtenerPreguntas } from "./Scripts/controladores/controllers.js";
-
-
-let header = document.getElementById("header");
+//----------------------importar funciones ------------------//
+import { urlAPI,obtenerPreguntas } from "./controllers/controllers.js";
+//---------------------- variables html --------------------//
+let selectNum = document.getElementById("trivia_amount");
+let selectCategoria = document.getElementById("form-control");
+let selectDificultad = document.getElementById("selectDificultad");
+let selectTipo = document.getElementById("selectTipo");
+let btnInicio = document.getElementById("Btn-Inicio");
+let bntCrear=document.getElementById("btn-crear")
+let barSuper = document.getElementById("barSuper");
 let pregunta = document.getElementById("pregunta");
 let respuestas = document.getElementById("respuestas");
-let informacion = await obtenerPreguntas();
-
-let a = 0;
-let numero = 1; // depende del numero que elija el usuario
-
-
-
-
-//----------------------------------------------FUNCIONES-----------------------------------------//
-function revisarInfo(num){
-  var boton = document.getElementById("btn-r",num)
-  var info = boton.innerHTML;
-    console.log(info)
-    return(info)
-};
-
-
-
-//console.log("--------------------------------------------------")
-//console.log(informacion)
-
-//--------------- Diseño de pantalla por cada pregunta -----------------------------//
-// 1- Crear un ciclo for para que se muestre en pantalla una a una las preguntas
-for (let i = 0; i < informacion.length; i++) {
-
-  let opciones = [];
-  let opcionesSort = "";
-  // 2- Verificar si el tipo de prefunta es multipe o falso y verdadero//
-  // Si es tipo falso se crear un vector con las posibles respuestas
-  if ((informacion[i].type = "multiple")) {
-    opciones.push(informacion[i].correct_answer);
-    for (let a = 0; a < informacion[i].incorrect_answers.length; a++) {
-      opciones.push(informacion[i].incorrect_answers[a]);
-    }
-  }
-  // si es de tipo verdadero/ falso se crea un vector con 2 opciones
-  else {
-    opciones.push("False", "True");
-  }
-  // Organizar en orden aleatorio las respuesta
-  opcionesSort = opciones.sort(function () {
-    return Math.random();
-  });
-  console.log("------");
-  //console.log("las opciones antes del sort son : ", opciones);
-  //console.log("las opciones despues del sort son : ", opcionesSort);
-
-  // 3-mostrar en pantalla pregunta y opciones de respuesta
-  header.innerHTML = `
-    <div class="nomCategoria" >${informacion[i].category}</div>
-    <div class="nomdificultad" >Dificultad: ${informacion[i].difficulty}</div> `;
-  pregunta.innerHTML +=  `${informacion[i].question}`;
-
-  for (let i = 0; i < opcionesSort.length; i++) {
-     respuestas.innerHTML +=
-       `<button id="btn-r${i}" class="nomRespuestas btn btn-outline-light" onclick="revisarInfo(${i})" value="${opcionesSort[i]}">${opcionesSort[i]}</button>
-     `;
-     
-  }
- 
-  opciones = [];
+let cartas = document.getElementById("cards");
+//-----------------------variables locales-------------------//
+let API = "";
+let informacion =""
+//-----------------------funciones-------------------------//
+// capturar la informacion de los select del header del html a traves del boton btnInicio
+export async function datosParaApi() {
+  let dataApi = [
+    parseInt(selectNum.value),
+    selectCategoria.value,
+    selectDificultad.value,
+    selectTipo.value,
+  ];
+  urlAPI(dataApi);
+  
 }
 
+ btnInicio.addEventListener("click", async function (event) {
+  informacion= await datosParaApi();
+ 
+  
+});
+
+
+
+/* array de prueba -------------------------------*/
+informacion= [
+    {
+      category: "Entertainment: Film",
+      type: "multiple",
+      difficulty: "medium",
+      question: "In the &quot;Jurassic Park&quot; universe, what is the name of the island that contains InGen&#039;s Site B?",
+      correct_answer: "Isla Sorna",
+      incorrect_answers: [
+        "Isla Nublar",
+        "Isla Pena",
+        "Isla Muerta"
+      ]
+    }
+  ]
+
+  
+// crear las cartas que contienen las preguntas y las respuestas
+async function cartaPreguntas() {
+  for (let i = 0; i <  informacion.length + 1; i++) {
+    let opciones = [];
+    let opcionesSort = "";
+    // 2- Verificar si el tipo de prefunta es multipe o falso y verdadero//
+    // Si es tipo falso se crear un vector con las posibles respuestas
+    if ((informacion[i].type = "multiple")) {
+      opciones.push(informacion[i].correct_answer);
+      for (let a = 0; a < informacion[i].incorrect_answers.length; a++) {
+        opciones.push(informacion[i].incorrect_answers[a]);
+      }
+    }
+    // si es de tipo verdadero/ falso se crea un vector con 2 opciones
+    else {
+      opciones.push("False", "True");
+    }
+    // Organizar en orden aleatorio las respuesta
+    opcionesSort = opciones.sort(function () {
+      return Math.random();
+    });
+    console.log("------");
+    //console.log("las opciones antes del sort son : ", opciones);
+    //console.log("las opciones despues del sort son : ", opcionesSort);
+
+    // 3-mostrar en pantalla pregunta y opciones de respuesta
+    cartas.innerHTML += `
+    <p class="nomCategoria" >${informacion[i].category}</p>
+    <p class="nomdificultad" >Dificultad: ${informacion[i].difficulty}</p> 
+    <p class="pregunta">${informacion[i].question}</p>
+   `
+    for (let i = 0; i < opcionesSort.length; i++) {
+        cartas.innerHTML +=`   <div class= "botones">
+        <button id="btn-r${i}" class="nomRespuestas btn btn-outline-light" onclick="revisarInfo(${i})" value="${opcionesSort[i]}">${opcionesSort[i]}</button>
+    
+    </div>`;
+  }
+}}
+btnInicio.addEventListener("click", async function (event) {
+await cartaPreguntas();
+  });
+  cartaPreguntas()
