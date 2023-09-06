b0575a2bd84d605671db27205b337870318f8c69
LauraChacon <laurachacon.ve@gmail.com>
2023-09-05 21:02:14 -0500

    commit

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,24 +4,21 @@ let preguntas=""
 export async function urlAPI (armarApi) {
 //1.Verificar que los valores en any o NaN queden vacios.  
   for (let index = 0; index < armarApi.length; index++) {
-    if (armarApi[index]==="any"|| armarApi[index]==="NaN")//Revisar cuando categoria = NaN
+    if (armarApi[index]==="any")
      {
       armarApi[index]=""  
     }
   }
 // 2. Variables para armar la api
   let numero = parseInt(armarApi[0])
-  let categoria =parseInt(armarApi[1])
+  let categoria =(armarApi[1])
   let dificultad = armarApi[2]
   let tipoRespuesta = armarApi[3]
  
   let triviaApi = `https://opentdb.com/api.php?amount=${numero}&category=${categoria}&difficulty=${dificultad}&type=${tipoRespuesta}`;
   preguntas = await fetch(triviaApi);
   let preguntasParseadas = await preguntas.json();
-  //console.log(preguntasParseadas)
   let preguntasArray=preguntasParseadas.results
-  //console.log("------------------------------------------------")
-  console.log("Preguntas en la funcion urlAPI ",preguntasArray)
   return preguntasArray;}
   
 
@@ -46,21 +43,3 @@ export async function urlAPI (armarApi) {
 
 
   
-  //console.log(triviaApi)
-  //return obtenerPreguntas(triviaApi)
-  /*
-// 3. Parsear información de la API
-export async function obtenerPreguntas(triviaApi) {
-  console.log("Ejecutando Obtener preguntas: ", triviaApi)
-    let preguntas = await fetch(triviaApi);
-  let preguntasParseadas = await preguntas.json();
-  //console.log(preguntasParseadas)
-  let preguntasArray=preguntasParseadas.results
-  //console.log("------------------------------------------------")
-  console.log("Preguntas ",preguntasArray)
-  return preguntasArray;
-}
-//--------------------------------------------------------------------------------------//
-
-
-*/
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //----------------------importar funciones ------------------//
 //import { urlAPI,obtenerPreguntas } from "./controllers/controllers.js";
-import { urlAPI} from "./controllers/controllers.js";
+import { urlAPI } from "./controllers/controllers.js";
 //---------------------- variables html --------------------//
 let selectNum = document.getElementById("trivia_amount");
 let selectCategoria = document.getElementById("form-control");
@@ -11,7 +11,7 @@ let btnInicio = document.getElementById("Btn-Inicio");
 let cartas = document.getElementById("cards");
 //-----------------------variables locales-------------------//
 let API = "";
-let informacion =""
+let informacion = "";
 //-----------------------funciones-------------------------//
 // capturar la informacion de los select del header del html a traves del boton btnInicio
 //export async function datosParaApi() {
@@ -23,29 +23,30 @@ async function datosParaApi() {
     selectTipo.value,
   ];
   //API = urlAPI(dataApi);
- // console.log("la data API en la funcion datos para API",dataApi)
-  return dataApi
+  console.log("la data API en la funcion datos para API", dataApi);
+  return dataApi;
 }
-  
+
 // crear las cartas que contienen las preguntas y las respuestas
-async function cartaPreguntas( informacion) {
+async function cartaPreguntas(informacion) {
+  cartas.innerHTML = "";
   //revisado y aprovado--------------
-  console.log("la informacion en funcion cartaspreguntas",informacion)
-  for (let i = 0; i < informacion.length ; i++) {
+  console.log("la informacion en funcion cartaspreguntas", informacion);
+  for (let i = 0; i < informacion.length; i++) {
     let opciones = [];
     let opcionesSort = "";
-    console.log("la informacion en funcion cartaspreguntas",informacion[i])
+    //console.log("la informacion en funcion cartaspreguntas", informacion[i]);
     // 2- Verificar si el tipo de prefunta es multipe o falso y verdadero//
     // Si es tipo falso se crear un vector con las posibles respuestas
     if ((informacion[i].type = "multiple")) {
       opciones.push(informacion[i].correct_answer);
-      console.log("la informacion de opciones",opciones)
+      console.log("la informacion de opciones", opciones);
       for (let a = 0; a < informacion[i].incorrect_answers.length; a++) {
         opciones.push(informacion[i].incorrect_answers[a]);
       }
     }
     // -----------------------------------------------------
-    
+
     // si es de tipo verdadero/ falso se crea un vector con 2 opciones
     else {
       opciones.push("False", "True");
@@ -54,45 +55,31 @@ async function cartaPreguntas( informacion) {
     opcionesSort = opciones.sort(function () {
       return Math.random();
     });
-    console.log("------");
-    //console.log("las opciones antes del sort son : ", opciones);
-    console.log("las opciones despues del sort son : ", opcionesSort);
-    
-// 3-mostrar en pantalla pregunta y opciones de respuesta
-    
-cartas.innerHTML += `
-<div class="barSuper">
-<p class="nomCategoria" >${informacion[i].category}</p>
-<p class="nomdificultad" >Dificultad: ${informacion[i].difficulty}</p> 
-</div>
-<p class="pregunta">${informacion[i].question}</p>
-`
-for (let i = 0; i < opcionesSort.length; i++) {
-    cartas.innerHTML +=`   <div class= "botones">
-    <button id="btn-r${i}" class="nomRespuestas btn btn-outline-light" value="${opcionesSort[i]}">${opcionesSort[i]}</button>
 
-</div>
-`;
+    // 3-mostrar en pantalla pregunta y opciones de respuesta
+
+    cartas.innerHTML += `
+        <div class="barSuper">
+        <p class="nomCategoria" >${informacion[i].category}</p>
+        <p class="nomdificultad" >Dificultad: ${informacion[i].difficulty}</p> 
+        </div>
+          <p class="pregunta">${informacion[i].question}</p>`;
+    for (let i = 0; i < opcionesSort.length; i++) {
+      cartas.innerHTML += `   
+                  <div class= "botones">
+                  <button id="btn-r${i}" 
+                  class="nomRespuestas btn btn-outline-light" 
+                  value="${opcionesSort[i]}">${opcionesSort[i]}</button>
+        </div>
+        `;
+    }
+  }
 }
-}}
 
 btnInicio.addEventListener("click", async function (event) {
-  API=await datosParaApi()
-  informacion=await urlAPI(API)
-  console.log("la informacion",informacion) 
+  API = await datosParaApi();
+  informacion = await urlAPI(API);
+  console.log("la informacion", informacion);
 
-  cartaPreguntas(informacion) 
-
+  cartaPreguntas(informacion);
 });
-
-
-
-
-
-
-  
-
-
- /*
-
-    */
